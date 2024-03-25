973c255584d0788fa156bba7bd701735b78e9275
lahcen <lahcen.ben3@gmail.com>
2024-03-25 15:20:37 +0000

    Refactor CSS and update component structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import Router from './router.jsx'
+import { ContextProvider } from './contexts/ContextProvider.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  <ContextProvider>
+    <RouterProvider router={Router}/>
+    </ContextProvider>
   </React.StrictMode>,
 )
