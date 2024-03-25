79252386be64ae5381edd1b37a7bc7e686209cdb
lahcen <lahcen.ben3@gmail.com>
2024-03-25 15:20:11 +0000

    Add new views: Dashboard, Home, Login, NotFound, and Register

---
+++ src/views/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div>
+        <h1>Home</h1>
+        <p>Welcome to the home page!</p>
+        </div>
+    );
+    }
+    
---
+++ src/views/Register.jsx
@@ -0,0 +1,15 @@
+export default function Register() {
+    return (
+        <div>
+        <h1>Register</h1>
+        <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Confirm Password" />
+            <button type="submit">Register</button>
+        </form>
+        </div>
+        
+    );
+    }
