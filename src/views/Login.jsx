d1f464b0a31b1534bcd604378def601febf72f6e
lahcen <lahcen.ben3@gmail.com>
2024-03-26 14:50:22 +0000

    Refactor Login component to use hooks and axios-client

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,28 +1,62 @@
-import React from 'react';
-import { Container, Form, Button } from 'react-bootstrap';
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom/dist";
+import { useStateContext } from "../contexts/ContextProvider";
+import axiosClient from "../axios-client";
 
 export default function Login() {
-    return (
-        <Container>
-            <Form style={{ maxWidth: '400px', margin: '0 auto' }}>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const { setUser, setToken } = useStateContext();
+  const [errors, setErrors] = useState(null);
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
+  function onSubmit(event) {
+    event.preventDefault();
 
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
-        </Container>
-    );
+    const payload = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
+
+    axiosClient
+      .post("/login", payload)
+      .then((response) => {
+        setUser(response.data.user);
+        setToken(response.data.token);
+        console.log(response.data.token);
+      })
+      .catch((error) => {
+        const response = error.response;
+        console.log(response.data.errors);
+        setErrors(response.data.errors);
+      });
+  }
+
+  return (
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <h1 className="title">Login into your account</h1>
+          <div>
+            {errors && (
+              <div className="alert alert-danger">
+                {Object.keys(errors).map((key) => (
+                  <div key={key}>
+                    {errors[key].map((message) => (
+                      <div key={message}>{message}</div>
+                    ))}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+          <input ref={emailRef} type="email" placeholder="Email" />
+          <input ref={passwordRef} type="password" placeholder="Password" />
+          <button className="btn btn-block">Login</button>
+          <p className="message">
+            Not registered? <Link to="/register">Create an account</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 }
-  
