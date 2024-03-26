c0bb225e5107cf0614ace3506f06fef437a3bc8a
lahcen <lahcen.ben3@gmail.com>
2024-03-26 14:49:55 +0000

    Add axios client with interceptors

---
+++ src/axios-client.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const axiosClient = axios.create({
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
+});
+
+axiosClient.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+axiosClient.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response.status === 401) {
+            localStorage.removeItem("token");
+            window.location.href = "/login";
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default axiosClient;
