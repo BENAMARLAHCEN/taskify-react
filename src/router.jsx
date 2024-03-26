d5811a22f615383b5b8286632583c2c511f27edd
lahcen <lahcen.ben3@gmail.com>
2024-03-26 14:50:06 +0000

    Add routes for Tasks and TaskForm components

--- src/router.jsx
+++ src/router.jsx
@@ -6,6 +6,8 @@ import NotFound from "./views/NotFound.jsx";
 import GuestLayout from "./components/GuestLayout.jsx";
 import Layout from "./components/Layout.jsx";
 import Dashboard from "./views/Dashboard.jsx";
+import Tasks from "./views/Tasks.jsx";
+import TaskForm from "./views/TaskForm.jsx";
 
 const Router = createBrowserRouter([
     {
@@ -35,6 +37,18 @@ const Router = createBrowserRouter([
                 path: "/dashboard",
                 element: <Dashboard />
             },
+            {
+                path: "/tasks",
+                element: <Tasks />
+            },
+            {
+                path: "/tasks/new",
+                element: <TaskForm key="TaskCreate" />
+            },
+            {
+                path: "/tasks/:id",
+                element: <TaskForm key="TaskUpdate" />
+            }
         ]
     },
     
